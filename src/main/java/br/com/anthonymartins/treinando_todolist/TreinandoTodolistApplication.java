135dda496f4968b42f6a728aa34cddc661ba5e19
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2024-09-04 10:30:52 -0300

    Normaliza quebras de linha em arquivos existentes

---
+++ src/main/java/br/com/anthonymartins/treinando_todolist/TreinandoTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.anthonymartins.treinando_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinandoTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinandoTodolistApplication.class, args);
+	}
+
+}
